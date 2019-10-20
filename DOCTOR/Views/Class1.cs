69f1adf685c0f0dad7a9e1819faaf58efa03b180
KaraboNkgoatau <50372486+KaraboNkgoatau@users.noreply.github.com>
2019-10-20 19:57:22 +0200

    Add files via upload

---
+++ DOCTOR/Views/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DOCTOR.ViewModel
+{
+    public class Class1
+    {
+        public class Prescription
+        {
+            public string Prescription_Date { get; set; }
+            public string PrescriptionLines { get; set; }
+            //public string lastName { get; set; }
+        }
+        public class RootObject
+        {
+            public List<Prescription> Lectures { get; set; }
+        }
+    }
+}
